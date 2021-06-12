44876fd4a3b288376524162760eaf20833ab58cc
Steven Horn <steven@horn.work>
2021-06-12 12:44:20 -0300

    return server / pool name and description

--- pool/pool_server.py
+++ pool/pool_server.py
@@ -59,7 +59,7 @@ class PoolServer:
         return inner
 
     async def index(self, _) -> web.Response:
-        return web.Response(text="Chia reference pool")
+        return web.Response(text=self.pool.info_name + " " + self.pool.info_description)
 
     async def get_pool_info(self, _) -> web.Response:
         res: PoolInfo = PoolInfo(
